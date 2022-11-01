36657295a5851aef1e67edb54050e7002e525dd2
Max <58262387+Pyr0maniac1@users.noreply.github.com>
2022-11-01 19:30:38 -0400

    Actually got it working

--- script.js
+++ script.js
@@ -1,51 +1,78 @@
+let computerScore = 0; 
+let playerScore = 0;  
+let moves = 10;
+const lMove = document.getElementById("Moves");
+const pScore = document.getElementById("P-count");
+const cScore = document.getElementById("C-count");
+const text = document.getElementById("Output");
+
+const game =() =>{
+
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
 
-let computerScore = 1; 
-let PlayerScore = 1; 
-const pScore = document.getElementById("PlayerScore");
-const cScore = document.getElementById("ComScore");
-
-if (computerChoice < 0.34) {
+if (computerChoice <= 0.34) {
 computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
+}else if(computerChoice <= 0.67) {
 computerChoice = "paper";
-} else {
+}else {
 computerChoice = "scissors";
 }
-var compare=function(choice1,choice2){
-if(choice1===choice2){
-return "The result is a tie!";
+function compare(choice1, choice2) {
+    if (choice1 === choice2) {
+
+        pScore.innerHTML = pScore.innerHTML;
+        cScore.innerHTML = cScore.innerHTML;
+        text.innerHTML = "Tie";
+        text.style.color = 'yellow';
+      
+
+    }else if (choice1 = "rock") {
+        if (choice2 === "scissors") {
+            playerScore++;
+            pScore.innerHTML = playerScore;
+            text.innerHTML = "Player wins!";
+            text.style.color = 'green';
+
+        }else if (choice2 === "paper") {
+
+            computerScore++;
+            cScore.innerHTML = computerScore;
+            text.innerHTML = "The computer wins!";
+            text.style.color = 'red';
+        }else if (choice1 = "paper") {
+            if (choice2 === "scissors") {
+                playerScore++;
+                pScore.innerHTML = playerScore;
+                text.innerHTML = "The user wins!";
+                text.style.color = 'green';
+                return "scissors win";
+
+            }else if (choice2 === "rock") {
+                computerScore++;
+                cScore.innerHTML = computerScore;
+                text.innerHTML = "The computer wins!";
+                text.style.color = 'red';
+                return "paper wins";
+            }
+        }else if (choice1 = "scissors") {
+            if (choice2 === "paper") {
+                playerScore++;
+                pScore.innerHTML = playerScore;
+                text.innerHTML = "The user wins!";
+                text.style.color = 'green';
+                return "scissors win";
+            }else if (choice2 === "rock") {
+                computerScore++;
+                cScore.innerHTML = computerScore;
+                text.innerHTML = "The computer wins!";
+                text.style.color = 'red';
+                return "rock wins";
+            }
+        }
+    }
+moves--; 
+lMove.innerHTML = moves;
 }
-else if(choice1="rock"){
-if(choice2==="scissors"){
-return "rock wins"
-PlayerScore++; 
+compare(userChoice,computerChoice);
 }
-else if(choice2==="paper"){
-return "paper wins";
-computerScore++;
-}
-}
-else if(choice1="paper"){
-if(choice2==="scissors"){
-return "scissors win"
-computerScore++;
-}
-else if(choice2==="rock"){
-return "paper wins";
-PlayerScore++; 
-}
-}
-else if(choice1="scissors"){
-if(choice2==="paper"){
-return "scissors win"
-PlayerScore++
-}
-else if(choice2==="rock"){
-return "rock wins";
-computerScore++; 
-}
-}
-}
-console.log(compare(userChoice,computerChoice));
