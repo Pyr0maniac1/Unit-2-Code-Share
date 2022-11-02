9c1f04920583aab73cf0b6a271be92a691a91e3f
Max <58262387+Pyr0maniac1@users.noreply.github.com>
2022-11-01 21:08:39 -0400

    all done :-)

--- script.js
+++ script.js
@@ -7,72 +7,61 @@ const cScore = document.getElementById("C-count");
 const text = document.getElementById("Output");
 
 const game =() =>{
-
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-
+    var userChoice = prompt("Do you choose rock, paper or scissors?");
+    var computerChoice = Math.random();
+    
 if (computerChoice <= 0.34) {
-computerChoice = "rock";
+    computerChoice = "rock";
 }else if(computerChoice <= 0.67) {
-computerChoice = "paper";
+    computerChoice = "paper";
 }else {
-computerChoice = "scissors";
+    computerChoice = "scissors";
 }
 function compare(choice1, choice2) {
     if (choice1 === choice2) {
-
         pScore.innerHTML = pScore.innerHTML;
         cScore.innerHTML = cScore.innerHTML;
         text.innerHTML = "Tie";
-        text.style.color = 'yellow';
-      
-
-    }else if (choice1 = "rock") {
+        text.style.color = 'yellow';  
+    } else if (choice1 == "rock") {
         if (choice2 === "scissors") {
             playerScore++;
             pScore.innerHTML = playerScore;
             text.innerHTML = "Player wins!";
             text.style.color = 'green';
-
         }else if (choice2 === "paper") {
-
             computerScore++;
             cScore.innerHTML = computerScore;
             text.innerHTML = "The computer wins!";
             text.style.color = 'red';
-        }else if (choice1 = "paper") {
+        }
+    } else if (choice1 == "paper") {
             if (choice2 === "scissors") {
                 playerScore++;
                 pScore.innerHTML = playerScore;
                 text.innerHTML = "The user wins!";
                 text.style.color = 'green';
-                return "scissors win";
-
             }else if (choice2 === "rock") {
                 computerScore++;
                 cScore.innerHTML = computerScore;
                 text.innerHTML = "The computer wins!";
                 text.style.color = 'red';
-                return "paper wins";
             }
-        }else if (choice1 = "scissors") {
+    } else if (choice1 == "scissors") {
             if (choice2 === "paper") {
                 playerScore++;
                 pScore.innerHTML = playerScore;
                 text.innerHTML = "The user wins!";
                 text.style.color = 'green';
-                return "scissors win";
             }else if (choice2 === "rock") {
                 computerScore++;
                 cScore.innerHTML = computerScore;
                 text.innerHTML = "The computer wins!";
                 text.style.color = 'red';
-                return "rock wins";
             }
         }
     }
-moves--; 
-lMove.innerHTML = moves;
-}
 compare(userChoice,computerChoice);
+    moves--; 
+    lMove.innerHTML = moves;
 }
